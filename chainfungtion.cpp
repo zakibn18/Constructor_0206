94e96fbeaff6b1a60cee8f111baa1517313d48a6
zakibn18 <jekben815@gmail.com>
2025-05-17 15:11:00 +0700

    membuat chain fungtion

--- chainfungtion.cpp
+++ chainfungtion.cpp
@@ -3,10 +3,24 @@ using namespace std;
 
 class buku
 {
-  
-};
+  string judul;
+
+  public:
+    buku setJudul(string judul)
+    {
+      this->judul = judul;
+      return *this;
+    }
+    string getJudul()
+    {
+      return this->judul;
+    }
+} bukunya;
 
 int main()
 {
-
+  // bukunya.setJudul("Matematika");
+  // cout << bukunya.getJudul();
+  cout << bukunya.setJudul("Matematika").getJudul(); // chain fungtion calls
+  return 0;
 }
